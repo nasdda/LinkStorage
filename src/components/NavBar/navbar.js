c9a95b9f367d850d17c161864fcf8db9c4860ba7
Xin <xin2001yan@gmail.com>
2020-08-08 19:24:04 -0700

    added temp navbar component

---
+++ src/components/NavBar/navbar.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles, createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+import { purple } from '@material-ui/core/colors';
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box p={3}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
+
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper,
+    },
+}));
+
+
+
+
+
+const theme = createMuiTheme(
+    {
+        palette: {
+            primary: {
+                main: '#1b5e20',
+            },
+            secondary: {
+                main: '#fdd835',
+            },
+        },
+    }
+);
+
+
+export default function SimpleTabs() {
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        console.log(newValue)
+        setValue(newValue);
+    };
+
+    return (
+        <div className={classes.root}>
+            <ThemeProvider theme={theme}>
+                <AppBar position="static">
+                    <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+                        <Tab label="Home" {...a11yProps(0)} />
+                        <Tab label="About" {...a11yProps(1)} />
+                    </Tabs>
+                </AppBar>
+                <TabPanel value={value} index={0}>
+                    Home
+                </TabPanel>
+                <TabPanel value={value} index={1}>
+                    About
+                </TabPanel>
+            </ThemeProvider>
+        </div>
+    );
+}
