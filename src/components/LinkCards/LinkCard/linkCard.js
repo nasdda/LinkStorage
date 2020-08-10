28b4349a3ebdd730571ddcc05c9814ffdb3318a1
Xin <xin2001yan@gmail.com>
2020-08-09 20:34:54 -0700

    update linkcard ui design

--- src/components/LinkCards/LinkCard/linkCard.js
+++ src/components/LinkCards/LinkCard/linkCard.js
@@ -8,19 +8,23 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Tooltip from '@material-ui/core/Tooltip'
+import Collapse from '@material-ui/core/Collapse'
+import IconButton from '@material-ui/core/IconButton';
+import clsx from 'clsx'
 
 // icons
-import { ReactComponent as Copy } from '../../../Icons/copy.svg'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import CopyIcon from '@material-ui/icons/FileCopyOutlined'
 
 import './linkCard.css'
 
 export default function MediaCard(props) {
     const [expand, setExpand] = useState(false)
 
-    const useStyles = makeStyles({
+    const useStyles = makeStyles(theme => ({
         root: {
-            minWidth: 200,
-            height: 280
+            minWidth: 250,
+            height: "100%"
         },
         media: { // image size should be 200 x 120
             width: 200,
@@ -30,10 +34,23 @@ export default function MediaCard(props) {
         content: {
             height: 80,
             overflowY: "hidden"
+        },
+        expand: {
+            transform: 'rotate(0deg)',
+            marginLeft: 'auto',
+            transition: theme.transitions.create('transform', {
+                duration: theme.transitions.duration.shortest,
+            }),
+        },
+        expandOpen: {
+            transform: 'rotate(180deg)',
         }
-    })
+    }))
     const classes = useStyles();
 
+    const handleExpandClick = () => {
+        setExpand(!expand);
+    };
     return (
         <Card className={classes.root}>
             <CardActionArea>
@@ -42,22 +59,36 @@ export default function MediaCard(props) {
                     image={props.image}
                     title={props.title}
                 />
-                <CardContent className={classes.content}>
-                    <Typography gutterBottom variant="h5" component="h2">
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2" align="center">
                         {props.title}
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        {props.description}
                     </Typography>
                 </CardContent>
             </CardActionArea>
             <CardActions>
                 <Tooltip title="copy link">
-                    <Button size="small" color="primary">
-                        <Copy />
-                    </Button>
+                    <IconButton aria-label="copy link">
+                        <CopyIcon />
+                    </IconButton>
+                </Tooltip>
+                <Tooltip title="show description">
+                    <IconButton
+                        className={clsx(classes.expand, {
+                            [classes.expandOpen]: expand,
+                        })}
+                        onClick={handleExpandClick}
+                        aria-expanded={expand}
+                        aria-label="show more"
+                    >
+                        <ExpandMoreIcon />
+                    </IconButton>
                 </Tooltip>
             </CardActions>
+            <Collapse in={expand} timeout="auto" unmountOnExit>
+                <Typography variant="body1" component="p">
+                    {props.description}
+                </Typography>
+            </Collapse>
         </Card >
     );
 }
--- src/components/LinkCards/LinkCards.js
+++ src/components/LinkCards/LinkCards.js
@@ -16,42 +16,42 @@ export default function (props) {
     return (
         <div className="outter">
             <Grid spacing={2} container>
-                <Grid item lg={2} md={4} sm={6} xs={12}>
+                <Grid item lg={3} md={4} sm={6} xs={12}>
                     <LinkCard
                         title="Techie Delight"
                         image={site}
                         description="A helpful site containing plethora of algorithm problems with solutions."
                     />
                 </Grid>
-                <Grid item lg={2} md={4} sm={6} xs={12}>
+                <Grid item lg={3} md={4} sm={6} xs={12}>
                     <LinkCard
                         title="Good Tool"
                         image={tool}
                         description="Tool for doing so and so. Very efficient."
                     />
                 </Grid>
-                <Grid item lg={2} md={4} sm={6} xs={12}>
+                <Grid item lg={3} md={4} sm={6} xs={12}>
                     <LinkCard
                         title="Video"
                         image={video}
                         description="A helpful video."
                     />
                 </Grid>
-                <Grid item lg={2} md={4} sm={6} xs={12}>
+                <Grid item lg={3} md={4} sm={6} xs={12}>
                     <LinkCard
                         title="React Documentary"
                         image={site}
                         description="Documentary of React."
                     />
                 </Grid>
-                <Grid item lg={2} md={4} sm={6} xs={12}>
+                <Grid item lg={3} md={4} sm={6} xs={12}>
                     <LinkCard
                         title="DFS Problem"
                         image={leetcode}
                         description="A worthwhile problem which deals which is best solved using DFS."
                     />
                 </Grid>
-                <Grid item lg={2} md={4} sm={6} xs={12}>
+                <Grid item lg={3} md={4} sm={6} xs={12}>
                     <LinkCard
                         title="Article"
                         image={article}
