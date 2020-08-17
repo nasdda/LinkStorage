ea7b27f0d3078dc0f0ee8d9385491a5beaacb55e
Xin <xin2001yan@gmail.com>
2020-08-16 20:23:31 -0700

    added error page for invalid route

--- src/Router/router.js
+++ src/Router/router.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import NavBar from "../components/NavBar/navbar";
 import Home from "../components/pages/home/home";
 import About from "../components/pages/about/about";
-
+import Error from "../components/pages/error/error"
 const AppRouter = () => (
   <BrowserRouter>
     <div>
@@ -12,6 +12,7 @@ const AppRouter = () => (
         <Switch>
           <Route path="/" component={Home} exact />
           <Route path="/about" component={About} />
+          <Route component={Error} />
         </Switch>
       </div>
     </div>
---
+++ src/components/pages/error/error.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './error.css'
+
+export default function(props) {
+    return (
+        <div className="error">
+            <p>
+                Page not found.
+            </p>
+        </div>
+    )
+}
