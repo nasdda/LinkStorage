9de8025b30617e257496a38a882368e250a5fa69
Xin <xin2001yan@gmail.com>
2020-08-23 02:07:24 -0700

    merge

--- src/components/pages/home/NavButtons/navbuttons.js
+++ src/components/pages/home/NavButtons/navbuttons.js
@@ -3,8 +3,6 @@ import { connect } from 'react-redux'
 import { Button } from "@material-ui/core";
 import './navbuttons.css'
 function NavButtons(props) {
-<<<<<<< HEAD
-=======
     const style = {
         marginBottom: 30,
         width: 120,
@@ -12,7 +10,6 @@ function NavButtons(props) {
         fontSize: 24
     }
 
->>>>>>> 3b001c13f68066872f98d9c8e32c9d702164217f
     const toTop = () => {
         document.body.scrollTop = 0;
         document.documentElement.scrollTop = 0;
@@ -21,18 +18,7 @@ function NavButtons(props) {
     return (
         <div className="nav-buttons">
             <Button variant="contained"
-<<<<<<< HEAD
-                style={
-                    {
-                        marginBottom: 30,
-                        width: 120,
-                        height: 50,
-                        fontSize: 24
-                    }
-                }
-=======
                 style={style}
->>>>>>> 3b001c13f68066872f98d9c8e32c9d702164217f
                 onClick={event => {
                     props.onPrevPage()
                     toTop()
@@ -40,18 +26,7 @@ function NavButtons(props) {
             >prev</Button>
 
             <Button variant="contained"
-<<<<<<< HEAD
-                style={
-                    {
-                        marginBottom: 30,
-                        width: 120,
-                        height: 50,
-                        fontSize: 24
-                    }
-                }
-=======
                 style={style}
->>>>>>> 3b001c13f68066872f98d9c8e32c9d702164217f
                 onClick={event => {
                     props.onNextPage()
                     toTop()
